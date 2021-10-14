00f6d58ffa61caf92d3e9683d985016b6fe5f210
David_Gavilanes <davidgavilanesvelez@gmail.com>
2021-10-14 19:12:45 +0200

    added home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('template')
+
+@section('main')
+
+
+<div class="px-4 py-5 my-5 text-center">
+  <h1 class="display-5 fw-bold">Prueba Técnica Hotelinking</h1>
+  <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, veniam quis! Blanditiis quaerat harum adipisci laboriosam sit repellendus, ipsa temporibus veritatis inventore excepturi nihil iure fugit provident iste cum suscipit.</p>
+  </div>
+</div>
+
+@endsection
